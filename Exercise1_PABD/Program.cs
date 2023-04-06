30bea3b92c399e40a15d218c1acc996222412e26
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-04-06 09:21:15 +0700

    membuat method baca data database

--- Exercise1_PABD/Program.cs
+++ Exercise1_PABD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,21 @@ namespace Exercise1_PABD
         static void Main(string[] args)
         {
 
+        }
+
+        public void baca(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("Select * From pembeli", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+            r.Close();
         }
     }
 }
