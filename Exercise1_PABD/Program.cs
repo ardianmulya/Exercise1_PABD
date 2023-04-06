183183a25cc30b6994b1accf934aa5dca1294b5f
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-04-06 09:26:09 +0700

    membuat method insert data

--- Exercise1_PABD/Program.cs
+++ Exercise1_PABD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,20 @@ namespace Exercise1_PABD
                 Console.WriteLine();
             }
             r.Close();
+        }
+
+        public void insert(string id_pembeli, string nama_pembeli, string alamat, SqlConnection con)
+        {
+            string str = "";
+            str = "insert into pembeli (id_pembeli,nama_pembeli,alamat)values(@id,@nama,@alamat)";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("id", id_pembeli));
+            cmd.Parameters.Add(new SqlParameter("nama", nama_pembeli));
+            cmd.Parameters.Add(new SqlParameter("alamat", alamat));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil ditambahkan");
         }
     }
 }
